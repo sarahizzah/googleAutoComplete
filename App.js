8fcc0de4b4db9aac001e407256cb839edd094b2c
SitiSarah Saaid <sarah@socar.my>
2022-10-23 10:35:57 +0800

    fixing/amend chnages

--- App.js
+++ App.js
@@ -2,12 +2,11 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
-import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import friendsReducer from './src/components/main';
-import HomeScreen from './src/components/HomeScreen';
-import Trending from './src/components/Trending';
+import LocationScreen from './src/components/Containers/Screens/LocationScreen';
+import Trending from './src/components/Containers/Screens/Trending';
 
 const store = createStore(friendsReducer);
 const Stack = createStackNavigator();
@@ -18,7 +17,7 @@ export default class App extends React.Component {
       <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator>
-            <Stack.Screen name="Location" component={HomeScreen} />
+            <Stack.Screen name="Location" component={LocationScreen} />
             <Stack.Screen name="Trending" component={Trending} />
           </Stack.Navigator>
         </NavigationContainer>
